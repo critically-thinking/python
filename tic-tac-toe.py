cfe1d2af8507e050d5c4987a97df7798241b99d5
colinrodney <bigboi0000@gmail.com>
2024-05-28 20:57:47 -0500

    display_board() function working as intended per requirements

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,46 +7,48 @@
 xPlayer = "x"
 oPlayer = "o"
 #occupiedSpaces = []
+gameBoard=[[1,2,3],[4,5,6],[7,8,9]]
 
 # Draw game board
-def display_board():
+
     # should accept 1 parameter containing current status of board
     # print board status to console
-    board=[[1,2,3],[4,5,6],[7,8,9]]
-    
-    # i is for ROW num
-    for i in range(len(board)):
-        print("+-------+")
-        
-        # j is for column num
-        for j in range(len(board[i])):
-            
-            # print gameBoard[rowNum][columnNum]
-##            print(board[i][j])
-            return board    
+def display_board(board):
 
-
-
-# makeMove()
-# Computer always gets first move @ center of board  with symbol 'x' (gameBoard[1][1])
-def firstMove(board):
-    board[1][1] = xPlayer
-    board[0][1] = oPlayer
-    # no need to pop value from board, we can track winner based on index positions
-    return board
-
-def checkBoardStatus(board):
+    #Check board status
     count = 0
     #rows
     for i in range(len(board)):        
         #columns
         for j in range(len(board[i])):
-            if board[i][j] == 'x' or  board[i][j] == 'o':
-                print("Choose another space.")
+            if board[i][j] == xPlayer or  board[i][j] == oPlayer:
+                print("Choose an available space on the board")
+    return board
+        
             
             # print gameBoard[rowNum][columnNum]
 ##            print(board[i][j])
-                
+    
+    # i is for ROW num
+    #for i in range(len(board)):
+    #    print("+-------+")
+        
+        # j is for column num
+     #   for j in range(len(board[i])):
+            
+            # print gameBoard[rowNum][columnNum]
+##            print(board[i][j])
+      #      return board    
+
+
+
+# makeMove()
+# Computer always gets first move @ center of board  with symbol 'x' (gameBoard[1][1])
+""" def firstMove(board):
+    board[1][1] = xPlayer
+    board[0][1] = oPlayer
+    # no need to pop value from board, we can track winner based on index positions
+    return board """
 
 def countMoves(board):
     count = 0
@@ -76,15 +78,14 @@ def countMoves(board):
 
 # Main Function
 def main():
-    board = display_board()
+    board = display_board(gameBoard)
     print("STARTING GAME: game board" , board)
 
-    boardStatus= firstMove(board) #return game board after PC makes first moves
-    print("game board after first move", boardStatus)
+    #boardStatus= firstMove(board) #return game board after PC makes first moves
+    #print("game board after first move", boardStatus)
 
-    checkBoardStatus(board)
 
-    countMoves(board)
+    #countMoves(board)
 
     
 
